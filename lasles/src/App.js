0be3c7ebf6bc00342f0539bf8b7fd0fe75a93ef9
Nitesh121-maker <72297535+Nitesh121-maker@users.noreply.github.com>
2022-11-26 19:38:09 +0530

    React

---
+++ lasles/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { LaslesVPN } from './LaslesVPN';
+export const App = () => {
+  return (
+    <div className="App">
+      <LaslesVPN />
+
+    </div>
+  );
+}
+
+export default App;
---
+++ lasles/src/LaslesVPN.jsx
@@ -0,0 +1,260 @@
+import React from 'react'
+import './LaslesVPN.css';
+import img1 from './Assets/Illustration 1.svg'
+import icon1 from './Assets/Group 1216.svg'
+import icon2 from './Assets/Group 1215.svg'
+import icon3 from './Assets/Group 1217.svg'
+import img from './Assets/Illustration 2.svg'
+import img2 from './Assets/Group 1120.svg'
+import PlanImg from './Assets/Free.svg'
+import Hugeimg from './Assets/Huge Global.svg'
+import netflixlogo from './Assets/Mask Group.svg'
+import redditlogo from './Assets/Mask Group (1).svg'
+import amazonlogo from './Assets/Mask Group (2).svg'
+import discordlogo from './Assets/Mask Group (3).svg'
+import spotifylogo from './Assets/Mask Group (4).svg'
+import star from './Assets/Star.svg'
+import user1 from './Assets/Ellipse 175.png'
+import user2 from './Assets/Ellipse 175 (1).png'
+import user3 from './Assets/Ellipse 175 (2).png'
+import Facebook from './Assets/Facebook.svg'
+import Twitter from './Assets/Twitter.svg'
+import Inst from './Assets/Instagram.svg'
+import { GiHamburgerMenu } from 'react-icons/gi';
+export const LaslesVPN = () => {
+  return (
+    <>
+    <nav className='Main-Nav'>
+       <div className='Logo'>        
+           <svg width="17vh" height="8vh" viewBox="0 0 17 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+             <path d="M15.1075 6.90327L15.0973 6.89304C15.0964 6.89247 15.0958 6.8917 15.0954 6.89081L8.69592 0.491211L2.29654 6.89065C2.2958 6.89139 2.29465 6.89287 2.29465 6.89287L2.28409 6.9031C1.30553 7.88715 0.683981 9.17006 0.518242 10.5479C0.352502 11.9258 0.652086 13.3195 1.36937 14.5076C2.08665 15.6956 3.18046 16.6099 4.47693 17.1048C5.7734 17.5998 7.19812 17.6472 8.52461 17.2394C8.63623 17.2053 8.75554 17.2053 8.86717 17.2394C10.1936 17.647 11.6182 17.5996 12.9146 17.1046C14.211 16.6095 15.3047 15.6954 16.0219 14.5074C16.7391 13.3194 17.0388 11.9258 16.8731 10.548C16.7074 9.17023 16.0859 7.88732 15.1075 6.90327ZM4.57208 11.1233C4.54092 11.1544 4.51622 11.1915 4.49937 11.2321C4.48251 11.2728 4.47384 11.3164 4.47384 11.3604C4.48523 12.4899 4.80443 13.5948 5.39709 14.5564C5.41055 14.5782 5.41677 14.6038 5.41487 14.6293C5.41296 14.6548 5.40302 14.6791 5.38646 14.6987C5.36991 14.7183 5.34759 14.7321 5.3227 14.7382C5.29781 14.7443 5.27163 14.7424 5.24789 14.7328C4.80235 14.5453 4.39759 14.2729 4.05626 13.9307C3.35676 13.2311 2.96351 12.2824 2.96288 11.2931C2.96224 10.3038 3.35427 9.35458 4.05288 8.65409L8.69592 4.01149L13.3387 8.65409C14.0374 9.35449 14.4296 10.3037 14.4289 11.2931C14.4284 12.2824 14.035 13.2311 13.3355 13.9307C12.9942 14.2729 12.5894 14.5453 12.144 14.7328C12.1202 14.7424 12.094 14.7443 12.0691 14.7382C12.0442 14.7321 12.0219 14.7182 12.0053 14.6987C11.9888 14.6791 11.9788 14.6548 11.9769 14.6292C11.975 14.6037 11.9812 14.5781 11.9947 14.5563C12.5874 13.5948 12.9068 12.4899 12.9184 11.3604C12.9184 11.3164 12.9096 11.2728 12.8927 11.232C12.8757 11.1913 12.851 11.1544 12.8197 11.1232L10.6236 8.79385C10.6017 8.77558 10.5742 8.76559 10.5457 8.76559C10.5173 8.76559 10.4897 8.77558 10.4679 8.79385C10.446 8.81202 10.4313 8.83739 10.4262 8.8654C10.421 8.89342 10.4259 8.92225 10.44 8.94704C11.2929 10.4905 10.5594 12.7075 9.33635 13.9307L9.3299 13.9372C9.16109 14.1042 8.93319 14.1978 8.69575 14.1977C8.45828 14.1976 8.23044 14.1038 8.06175 13.9368L8.05581 13.9307C6.83227 12.7075 6.09922 10.4907 6.95227 8.94737C6.9663 8.92258 6.97119 8.89375 6.96608 8.86573C6.96098 8.83772 6.94622 8.81236 6.92436 8.79418C6.9025 8.77592 6.87495 8.76592 6.84648 8.76592C6.81801 8.76592 6.79045 8.77592 6.76859 8.79418L4.57208 11.1233Z" fill="#F53838"/>
+           </svg>
+
+           <h1>LaslesVPN</h1>
+       </div>
+       <div className='Nav-Links'> 
+         <ul>
+            <li>
+                <a href="#About">About</a>
+            </li>
+            <li>
+                <a href="#Features">Features</a>
+            </li>
+            <li>
+                <a href="#Pricing">Pricing</a>
+            </li>
+            <li>
+                <a href="#Testimonials">Testimonials</a>
+            </li>
+            <li>
+                <a href="#Help">Help</a>
+            </li>
+         </ul>
+         
+       </div>
+       <div className='Buttons'>
+           <div className="btn1"><button><h4>Sign In </h4></button></div>
+           <div className="btn2"> <button><h4>Sign Up</h4></button></div>
+           <div className="menu">
+              <a href="#"><GiHamburgerMenu/></a>
+            </div>
+       </div>
+       
+    </nav>
+    <div className='HeaderUp'>
+        <div className="heading">
+            <h2 className='headertag'>Want anything to be <br />easy with<span>LaslesVPN.</span></h2>
+            <p className='paragraph'>Provide a network for all your needs with ease and fun using <span>LaslesVPN</span><br /> discover interesting features from us.</p>
+            <button className='BtnStart'>Get Started</button>
+        </div>
+        <div className="pic">
+             <img src={img1} alt="" />
+        </div>
+    </div>
+    <div className="servicesinfo">
+        <div className="users">
+            <img src={icon1} alt="icon1" />
+            <h3>90+ <br /> <span>Users</span></h3>
+           
+        </div>
+        <div className="line1"></div>
+        <div className="Locations">
+            <img src={icon2} alt="icon2" />
+            <h3>30+ <br /><span>Locations</span></h3>
+            
+        </div>
+        <div className="line2"></div>
+        <div className="Servers">
+            <img src={icon3} alt="icon3" />  
+            <h3>50+ <br /><span>Servers</span></h3>           
+        </div>
+    </div>
+    {/*----------------------- Features=---------------- */}
+    <div className="features">
+        <div className="img">
+            <img src={img} alt="" />
+        </div>
+        <div className="info">
+            <h2>We Provide Many Features You Can Use</h2>
+            <p>You can explore the features that we provide with fun and have their own functions each feature.</p>
+            <div className="feature1">
+             <img src={img2} alt="1120" /><span>Powerfull online protection.</span><br /><br />
+             <img src={img2} alt="1120" /><span>Internet without borders.</span><br /><br />
+             <img src={img2} alt="1120" /><span>Supercharged VPN</span><br /><br />
+             <img src={img2} alt="1120" /><span>No specific time limits.</span><br /><br />
+            </div>
+        </div>
+    </div>
+    <section className='planMain'>
+        <div className='Plan'>
+            <h2>Choose Your Plan</h2>
+            <p>Let's choose the package that is best for you and explore it happily and <br /><span>cheerfully.</span> </p>
+        
+        </div>
+        <div className='Plan1'>
+            <div className="p1">
+                <div className="pimg"><img src={PlanImg} alt="" /></div>
+                <h3>Free Plan</h3>
+                <img src={img2} alt="1120" /><span>Unlimited Bandwitch</span><br /><br />
+                <img src={img2} alt="1120" /><span>Encrypted Connection</span><br /><br />
+                <img src={img2} alt="1120" /><span>No Traffic Logs</span><br /><br />
+                <img src={img2} alt="1120" /><span>Works on All Devices</span><br /><br />
+                <h3>Free</h3>
+                <button>Select</button>
+            </div>
+            <div className="p2">
+                <div className="pimg"><img src={PlanImg} alt="" /></div>
+                <h3>Free Plan</h3>
+                <img src={img2} alt="1120" /><span>Unlimited Bandwitch</span><br /><br />
+                <img src={img2} alt="1120" /><span>Encrypted Connection</span><br /><br />
+                <img src={img2} alt="1120" /><span>No Traffic Logs</span><br /><br />
+                <img src={img2} alt="1120" /><span>Works on All Devices</span><br /><br />
+                <h3>$9 / mo</h3>
+                <button>Select</button>
+            </div>
+            <div className="p3">
+                <div className="pimg"><img src={PlanImg} alt="" /></div>
+                <h3>Free Plan</h3>
+                <img src={img2} alt="1120" /><span>Unlimited Bandwitch</span><br /><br />
+                <img src={img2} alt="1120" /><span>Encrypted Connection</span><br /><br />
+                <img src={img2} alt="1120" /><span>No Traffic Logs</span><br /><br />
+                <img src={img2} alt="1120" /><span>Works on All Devices</span><br /><br />
+                <h3>$12 / mo</h3>
+                <button>Select</button>
+            </div>
+        </div>
+    </section>
+    <section>
+       <div className='Global'>
+            <h2>Huge Global Network <br /><span>of Fast VPN</span> </h2>
+            <p>See LaslesVPN everywhere to make it easier for you when you move <br /> <span>locations.</span> </p>
+        </div>
+        <div className="map">
+            <img src={Hugeimg} alt="" />
+        </div>
+    </section>
+     <div className="siteName">
+            <img src={netflixlogo} alt="" />
+            <img src={redditlogo} alt="" />
+            <img src={amazonlogo} alt="" />
+            <img src={discordlogo} alt="" />
+            <img src={spotifylogo} alt="" />
+     </div>
+     <div className="testimonials">
+        <div className='Reviews'>
+            <h2>Trusted by Thousands of <br /><span> Happy Customer </span></h2>
+            <p>These are the stories of our customers who have joined us with great <br /><span> pleasure when using this crazy feature.</span> </p>
+        </div>
+        <div className="usersreview">
+            <div className="uv1">
+                <div className="uvs1">
+                   <img className='us1' src={user1} alt="" />
+                   <h3>Viezh Robert <br /> <span>Warsaw, Poland</span></h3>
+                   <span>4.5  <img src={star} alt="" /></span>
+                  
+                </div>
+                <div className="uvs2">
+                    <p>“Wow... I am very happy to use this VPN, it turned out to be more than my 
+                      expectations and so far there have been no problems. LaslesVPN always the best”.
+                    </p>
+                </div>
+            </div>
+            <div className="uv2">
+               <div className="uvs3">
+                   <img className='us2' src={user2} alt="" />
+                   <h3>Yessica Christy <br /> <span>Shanxi, China</span></h3>
+                   <span>4.5 <img src={star} alt="" /></span> 
+                   
+               </div>
+               <div className="uvs4">
+                   <p>“I like it because I like to travel far and still can connect with high speed.”.
+                    </p>
+               </div>
+            </div>
+            <div className="uv3">
+              <div className="uvs5">
+                  <img className='us3' src={user3} alt="" />
+                   <h3>Kim Young Jou <br /> <span>Seoul, South Korea</span></h3>
+                   <span>4.5 <img src={star} alt="" /></span>
+                   
+              </div>
+              <div className="uvs6">
+                   <p>“This is very unusual for my business that currently requires a virtual 
+                    private network that has high security.”.
+                    </p>
+              </div>
+            </div>
+        </div>
+     </div>
+     <div className="subscription">
+        <h3>Subscribe Now for <br /> Get Special Features! <br /><span>Let's subscribe with us and find the fun.</span></h3>
+        <button>Subscribe Now</button>
+     </div>
+     <div className="footer">
+        <div className="foot1">
+
+        </div>
+        <div className="foot2">
+         <div className='Logo'>        
+            <svg width="17vh" height="8vh" viewBox="0 0 17 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+             <path d="M15.1075 6.90327L15.0973 6.89304C15.0964 6.89247 15.0958 6.8917 15.0954 6.89081L8.69592 0.491211L2.29654 6.89065C2.2958 6.89139 2.29465 6.89287 2.29465 6.89287L2.28409 6.9031C1.30553 7.88715 0.683981 9.17006 0.518242 10.5479C0.352502 11.9258 0.652086 13.3195 1.36937 14.5076C2.08665 15.6956 3.18046 16.6099 4.47693 17.1048C5.7734 17.5998 7.19812 17.6472 8.52461 17.2394C8.63623 17.2053 8.75554 17.2053 8.86717 17.2394C10.1936 17.647 11.6182 17.5996 12.9146 17.1046C14.211 16.6095 15.3047 15.6954 16.0219 14.5074C16.7391 13.3194 17.0388 11.9258 16.8731 10.548C16.7074 9.17023 16.0859 7.88732 15.1075 6.90327ZM4.57208 11.1233C4.54092 11.1544 4.51622 11.1915 4.49937 11.2321C4.48251 11.2728 4.47384 11.3164 4.47384 11.3604C4.48523 12.4899 4.80443 13.5948 5.39709 14.5564C5.41055 14.5782 5.41677 14.6038 5.41487 14.6293C5.41296 14.6548 5.40302 14.6791 5.38646 14.6987C5.36991 14.7183 5.34759 14.7321 5.3227 14.7382C5.29781 14.7443 5.27163 14.7424 5.24789 14.7328C4.80235 14.5453 4.39759 14.2729 4.05626 13.9307C3.35676 13.2311 2.96351 12.2824 2.96288 11.2931C2.96224 10.3038 3.35427 9.35458 4.05288 8.65409L8.69592 4.01149L13.3387 8.65409C14.0374 9.35449 14.4296 10.3037 14.4289 11.2931C14.4284 12.2824 14.035 13.2311 13.3355 13.9307C12.9942 14.2729 12.5894 14.5453 12.144 14.7328C12.1202 14.7424 12.094 14.7443 12.0691 14.7382C12.0442 14.7321 12.0219 14.7182 12.0053 14.6987C11.9888 14.6791 11.9788 14.6548 11.9769 14.6292C11.975 14.6037 11.9812 14.5781 11.9947 14.5563C12.5874 13.5948 12.9068 12.4899 12.9184 11.3604C12.9184 11.3164 12.9096 11.2728 12.8927 11.232C12.8757 11.1913 12.851 11.1544 12.8197 11.1232L10.6236 8.79385C10.6017 8.77558 10.5742 8.76559 10.5457 8.76559C10.5173 8.76559 10.4897 8.77558 10.4679 8.79385C10.446 8.81202 10.4313 8.83739 10.4262 8.8654C10.421 8.89342 10.4259 8.92225 10.44 8.94704C11.2929 10.4905 10.5594 12.7075 9.33635 13.9307L9.3299 13.9372C9.16109 14.1042 8.93319 14.1978 8.69575 14.1977C8.45828 14.1976 8.23044 14.1038 8.06175 13.9368L8.05581 13.9307C6.83227 12.7075 6.09922 10.4907 6.95227 8.94737C6.9663 8.92258 6.97119 8.89375 6.96608 8.86573C6.96098 8.83772 6.94622 8.81236 6.92436 8.79418C6.9025 8.77592 6.87495 8.76592 6.84648 8.76592C6.81801 8.76592 6.79045 8.77592 6.76859 8.79418L4.57208 11.1233Z" fill="#F53838"/>
+            </svg>
+            <h1>LaslesVPN</h1>
+         </div>
+         <p><span>LaslesVPN</span>is a private virtual network that has unique features and has high security.</p>
+          <div className="iconsocialmedia">
+            <img src={Facebook} alt="" />
+            <img src={Twitter} alt="" />
+            <img src={Inst} alt="" />
+          </div>
+        </div>
+       <div className="foot">
+         <div className="Product">
+            <h4>Product</h4>
+            <span>Download</span><br />
+            <span>Pricing</span><br />
+            <span>Locations</span><br />
+            <span>Server</span><br />
+            <span>Countries</span><br />
+            <span>Blog</span>
+         </div>
+         <div className="Engage">
+            <h4>Engage</h4>
+            <span>LaslesVPN ? </span><br />
+            <span>FAQ</span><br />
+            <span>Tutorials</span><br />
+            <span>About Us</span><br />
+            <span>Privacy Policy</span><br />
+            <span>Terms of Service</span>
+         </div>
+         <div className="Earn">
+            <h4>Earn-Mony</h4>
+            <span>Affiliate</span><br />
+            <span>Become Partner</span><br />
+         </div>
+       </div>
+     </div>
+    </>
+    
+  )  
+}
